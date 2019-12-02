f1d886108a995e56285ea63f7d00127a18bdfac3
Phantasma_ <phantasmalspectre@aol.com>
2019-12-01 19:43:33 -0500

    Lap System

---
+++ NeonRacer/Assets/Scripts/CarData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarData : MonoBehaviour
+{
+    [HideInInspector] public int lapCount = 0;
+    [HideInInspector] public CheckPointScript.Checkpoint lastCheckPoint;
+    private void Start()
+    {
+        lastCheckPoint = CheckPointScript.Checkpoint.FinishLine;
+    }
+}
---
+++ NeonRacer/Assets/Scripts/CheckPointScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour
+{
+    public enum Checkpoint
+    {
+        First,
+        CheckPoint1,
+        CheckPoint2,
+        FinishLine,
+        Last
+    }
+    [SerializeField] private Checkpoint myCheckpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            CheckpointReached(other.GetComponent<CarData>(), myCheckpoint);
+        }
+    }
+    public void CheckpointReached(CarData player, Checkpoint sender)
+    {
+        switch (sender)
+        {
+            case Checkpoint.CheckPoint1:
+                if (Checkpoint.FinishLine == player.lastCheckPoint)
+                {
+                    player.lastCheckPoint = sender;
+                }
+                break;
+            case Checkpoint.CheckPoint2:
+                if (Checkpoint.CheckPoint1 == player.lastCheckPoint)
+                {
+                    player.lastCheckPoint = sender;
+                }
+                break;
+            case Checkpoint.FinishLine:
+                if (Checkpoint.CheckPoint2 == player.lastCheckPoint)
+                {
+                    player.lastCheckPoint = sender;
+                    player.lapCount++;
+                }
+                break;
+            default:
+                Debug.Log(sender);
+                break;
+        }
+    }
+}
