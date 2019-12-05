0c1364544295a926aaa2debed2ac636811b7900f
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-12-05 11:40:04 -0500

    Camera movement

---
+++ NeonRacer/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    Camera cam;
+    Vector3 average;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        average.x = 0;
+        average.z = 0;
+        for (int i = 0; i < References.players.Count; i++)
+        {
+            average.x += References.players[i].transform.position.x;
+            average.z += References.players[i].transform.position.z;
+        }
+        cam.transform.position = average / 4;
+        cam.transform.position = Vector3.MoveTowards(cam.transform.position, average / 4, .5f);
+    }
+}
